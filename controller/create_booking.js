cc717daa7289d6ace226e757b1413ceb2490e656
bagus priambudi <baguspriambudi77@gmail.com>
2020-12-17 10:16:25 +0700

    add: fitur send email in mailtrap.io

--- controller/create_booking.js
+++ controller/create_booking.js
@@ -1,9 +1,27 @@
+const nodemailer = require('nodemailer');
 const Booking = require('../models/booking');
 const Room = require('../models/room');
+const User = require('../models/user');
 const { httpOkResponse, httpNotFound, httpAuthenticationFailed } = require('../helper/http_respone');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'c9e816ffb3098f',
+    pass: '008256610cee80',
+  },
+});
 
 exports.bookingCreate = async (req, res, next) => {
   try {
+    const findEmail = await User.findByPk(req.user.id);
+    const mailOptions = {
+      from: 'baguspriambudi@gmail.com',
+      to: findEmail.email,
+      subject: 'testing',
+      text: 'success',
+    };
     const { room, time, noted } = req.body;
     const findRoom = await Room.findByPk(room);
     if (!findRoom) {
@@ -18,12 +36,12 @@ exports.bookingCreate = async (req, res, next) => {
       booking_time: time,
       noted,
     });
-    console.log(create.id);
     if (create) {
       await Room.update({ capacity: findRoom.capacity - 1 }, { where: { id: room } });
+      await transporter.sendMail(mailOptions);
       //   await Booking.update({ total_person: create.total_person + 1 }, { where: { id: create.id } });
     }
-    httpOkResponse(res, 'success', create);
+    httpOkResponse(res, 'success, please check your email', create);
   } catch (error) {
     next(error);
   }
